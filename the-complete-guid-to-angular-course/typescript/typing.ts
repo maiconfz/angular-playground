f366a2415d69bf569436a7b4b212953f7ee6c59a
Maiconfz <maiconfz@gmail.com>
2019-07-14 23:48:46 +0100

    Add type sample ts

---
+++ the-complete-guid-to-angular-course/typescript/typing.ts
@@ -0,0 +1,32 @@
+let myString: string;
+
+myString = 'Zanco';
+
+console.log(myString);
+
+let mySecondString = 'Maicon';
+
+console.log(mySecondString);
+
+let myNumber: number;
+let isBoolean: boolean;
+let myArray: Array<string>;
+let myAny: any;
+
+console.log(myNumber);
+console.log(isBoolean);
+console.log(myArray);
+console.log(myAny);
+
+myNumber = 29;
+isBoolean = true;
+myArray = new Array<string>();
+myArray[0] = 'Maicon';
+myArray[1] = 'Fonseca';
+myArray[2] = 'Zanco';
+myAny = 99;
+
+console.log(myNumber);
+console.log(isBoolean);
+console.log(myArray);
+console.log(myAny);
