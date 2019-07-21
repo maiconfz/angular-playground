e5d33a87bb2ea7616cd48e88c797c6ea1ea95689
Maiconfz <maiconfz@gmail.com>
2019-07-21 18:01:45 +0100

    Add inline-template component

--- the-complete-guid-to-angular-course/my-first-app/src/app/app.module.ts
+++ the-complete-guid-to-angular-course/my-first-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { InlineTemplateComponent } from './inline-template/inline-template.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    InlineTemplateComponent
   ],
   imports: [
     BrowserModule,
---
+++ the-complete-guid-to-angular-course/my-first-app/src/app/inline-template/inline-template.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inline-template',
+  template: '<p>This is a inline template code</p>'
+})
+export class InlineTemplateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
