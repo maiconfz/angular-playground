98e7bf8e6a58f326bb8ab78a59b29ea3c0760738
ZANCO Maicon Fonseca <mfzanco@europe.corp.altran.com>
2019-09-12 17:53:13 +0100

    Make getHeroes from HeroesService Observable

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -24,7 +24,7 @@ export class HeroesComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero) {
