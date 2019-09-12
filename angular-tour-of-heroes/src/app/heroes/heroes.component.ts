01eff22891bfbb83d63656275f9b51729160ecc1
ZANCO Maicon Fonseca <mfzanco@europe.corp.altran.com>
2019-09-12 17:46:02 +0100

    Add hero service

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,24 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  private heroService: HeroService;
+
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   }
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(heroService: HeroService) {
+    this.heroService = heroService;
+  }
 
   ngOnInit() {
+    this.heroes = this.heroService.getHeroes();
   }
 
   onSelect(hero: Hero) {
